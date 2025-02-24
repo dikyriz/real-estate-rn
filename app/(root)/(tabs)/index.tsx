d42719d7c50d4acbfe752c4487672094603d24ab
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2025-02-24 08:33:10 +0700

    creating deasign homescreen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,17 +1,33 @@
-import { Text, View } from "react-native";
+import {Image, SafeAreaView, Text, TouchableOpacity, View} from "react-native";
 import {Link} from "expo-router";
+import Images from "@/constants/images"
+import Icons from "@/constants/icons"
+import Search from "@/components/Search";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-3xl my-10 font-rubik">Welcome</Text>
-
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+          <View className="flex flex-row items-center justify-between mt-5">
+              <View className="flex flex-row items-center">
+                  <Image source={Images.avatar} className="size-12 rounded-full"/>
+                  <View className="flex flex-col items-start ml-2 justify-center">
+                      <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+                      <Text className="text-base font-rubik-medium text-black-300">Dicky</Text>
+                  </View>
+              </View>
+              <Image source={Icons.bell} className="size-6"/>
+          </View>
+          <Search/>
+          <View className="my-5">
+              <View className="flex flex-row items-center justify-between">
+                  <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+                  <TouchableOpacity>
+                      <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+                  </TouchableOpacity>
+              </View>
+          </View>
+      </View>
+    </SafeAreaView>
   );
 }
