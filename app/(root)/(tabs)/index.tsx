33c03a4314e7f5139aa1fc16cfef815a0a870572
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2025-02-25 22:03:16 +0700

    design homescreen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import {Link} from "expo-router";
 import Images from "@/constants/images"
 import Icons from "@/constants/icons"
 import Search from "@/components/Search";
+import {FeaturedCard, Card} from "@/components/Cards";
 
 export default function Index() {
   return (
@@ -26,6 +27,22 @@ export default function Index() {
                       <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
                   </TouchableOpacity>
               </View>
+              <View className="flex flex-row gap-5 mt-5">
+                  <FeaturedCard/>
+                  <FeaturedCard/>
+                  <FeaturedCard/>
+              </View>
+          </View>
+          <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
+              <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+              </TouchableOpacity>
+          </View>
+          <View className="flex flex-row gap-5 mt-5">
+              <Card/>
+              <Card/>
+              <Card/>
           </View>
       </View>
     </SafeAreaView>
