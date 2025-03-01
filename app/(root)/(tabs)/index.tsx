87bfe390458f97cf12548401bcfb36f3ad8445ed
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2025-03-01 08:24:16 +0700

    seed database

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {FlatList, Image, SafeAreaView, Text, TouchableOpacity, View} from "react-native";
+import {Button, FlatList, Image, SafeAreaView, Text, TouchableOpacity, View} from "react-native";
 import {Link} from "expo-router";
 import Images from "@/constants/images"
 import Icons from "@/constants/icons"
@@ -6,11 +6,13 @@ import Search from "@/components/Search";
 import {FeaturedCard, Card} from "@/components/Cards";
 import Filters from "@/components/Filters";
 import {useGlobalContext} from "@/lib/global-provider";
+import seed from "@/lib/seed";
 
 export default function Index() {
   const {user} = useGlobalContext();
   return (
     <SafeAreaView className="bg-white h-full">
+        <Button title="Seed" onPress={seed} />
         <FlatList data={[1, 2, 3, 4]} renderItem={({item}) => <Card/>} keyExtractor={(item) => item.toString()} numColumns={2} contentContainerClassName="pb-32" columnWrapperClassName="flex gap-5 px-5" showsVerticalScrollIndicator={false} ListHeaderComponent={<View className="px-5">
             <View className="flex flex-row items-center justify-between mt-5">
                 <View className="flex flex-row items-center">
@@ -30,7 +32,7 @@ export default function Index() {
                         <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
                     </TouchableOpacity>
                 </View>
-                <FlatList data={[1, 2, 3]} renderItem={(item) => <FeaturedCard/>} keyExtractor={(item) => item.toString()} horizontal bounces={false} showsHorizontalScrollIndicator={false} contentContainerClassName="flex gap-5 mt-5" />
+                <FlatList data={[5, 6, 7]} renderItem={(item) => <FeaturedCard/>} keyExtractor={(item) => item.toString()} horizontal bounces={false} showsHorizontalScrollIndicator={false} contentContainerClassName="flex gap-5 mt-5" />
             </View>
             <View className="flex flex-row items-center justify-between">
                 <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -6,11 +6,11 @@ export const config = {
     platform: "com.restate.app",
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-    databaseId: process.env.EXPO_PUBLIC_DATABASE_ID,
-    agentsCollectionId: process.env.EXPO_AGENTS_COLLECTION_ID,
-    galleriesCollectionId: process.env.EXPO_GALLERIES_COLLECTION_ID,
-    reviewsCollectionId: process.env.EXPO_REVIEWS_COLLECTION_ID,
-    propertiesCollectionId: process.env.EXPO_PROPERTIES_COLLECTION_ID,
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+    agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+    galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+    reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+    propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 }
 
 export const client = new Client();
