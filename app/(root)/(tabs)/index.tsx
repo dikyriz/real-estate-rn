8ba4a6a01cb3c4cbfc1609afee9e8ec4c089febf
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2025-03-02 22:56:54 +0700

    display data homepage

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,5 @@
-import {Button, FlatList, Image, SafeAreaView, Text, TouchableOpacity, View} from "react-native";
-import {Link} from "expo-router";
+import {ActivityIndicator, Button, FlatList, Image, SafeAreaView, Text, TouchableOpacity, View} from "react-native";
+import {Link, router, useLocalSearchParams} from "expo-router";
 import Images from "@/constants/images"
 import Icons from "@/constants/icons"
 import Search from "@/components/Search";
@@ -7,13 +7,27 @@ import {FeaturedCard, Card} from "@/components/Cards";
 import Filters from "@/components/Filters";
 import {useGlobalContext} from "@/lib/global-provider";
 import seed from "@/lib/seed";
+import {useAppwrite} from "@/lib/useAppWrite";
+import {getLatestProperties, getProperties} from "@/lib/appwrite";
+import {useEffect} from "react";
+import NoResults from "@/components/NoResults";
 
 export default function Index() {
   const {user} = useGlobalContext();
+  const params = useLocalSearchParams<{query?: string; filter?:string;}>();
+
+  const {data: latestProperties, loading: latestPropertiesLoading} = useAppwrite({fn: getLatestProperties});
+
+  const {data: properties, loading, refetch} = useAppwrite({fn: getProperties, params: {filter: params.filter!, query: params.query!, limit: 6}, skip: true});
+
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+
+    useEffect(() => {
+        refetch({filter: params.filter!, query: params.query!, limit: 6});
+    }, [params.filter, params.query]);
   return (
     <SafeAreaView className="bg-white h-full">
-        <Button title="Seed" onPress={seed} />
-        <FlatList data={[1, 2, 3, 4]} renderItem={({item}) => <Card/>} keyExtractor={(item) => item.toString()} numColumns={2} contentContainerClassName="pb-32" columnWrapperClassName="flex gap-5 px-5" showsVerticalScrollIndicator={false} ListHeaderComponent={<View className="px-5">
+        <FlatList data={properties} renderItem={({item}) => <Card item={item} onPress={()=>handleCardPress(item.$id)}/>} keyExtractor={(item) => item.toString()} numColumns={2} contentContainerClassName="pb-32" columnWrapperClassName="flex gap-5 px-5" ListEmptyComponent={loading ? (<ActivityIndicator size="large" className="text-primary-300 mt-5"/>) : <NoResults/>} showsVerticalScrollIndicator={false} ListHeaderComponent={<View className="px-5">
             <View className="flex flex-row items-center justify-between mt-5">
                 <View className="flex flex-row items-center">
                     <Image source={{uri: user?.avatar}} className="size-12 rounded-full"/>
@@ -32,7 +46,8 @@ export default function Index() {
                         <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
                     </TouchableOpacity>
                 </View>
-                <FlatList data={[5, 6, 7]} renderItem={(item) => <FeaturedCard/>} keyExtractor={(item) => item.toString()} horizontal bounces={false} showsHorizontalScrollIndicator={false} contentContainerClassName="flex gap-5 mt-5" />
+                {latestPropertiesLoading ? <ActivityIndicator size="large" className="text-primary-300"/> : !latestProperties || latestProperties.length === 0 ? <NoResults/> : (
+                <FlatList data={latestProperties} renderItem={({item}) => <FeaturedCard item={item} onPress={()=>handleCardPress(item.$id)}/>} keyExtractor={(item) => item.toString()} horizontal bounces={false} showsHorizontalScrollIndicator={false} contentContainerClassName="flex gap-5 mt-5" />)}
             </View>
             <View className="flex flex-row items-center justify-between">
                 <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
